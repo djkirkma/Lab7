281a0192c6214b02b4d9472faa0f4b7cda6b2afb
djkirkma <44447880+djkirkma@users.noreply.github.com>
2019-02-28 14:08:40 -0500

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,18 @@
+<?php
+public function camelCaseToUnderscore($str) {
+    $characters = str_split($str); //store all characters in an array
+    $characters[0] = strtolower($characters[0]); //covert the first character to a lowercase
+    //exam all characters in the array. If a character is uppercase, replace it with a lowercase and prefix it with an underscore.
+    foreach ($characters as &$character) {
+        if (ord($character) >= ord('A') && ord($character) <= ord('Z'))
+            $character = '_' . strtolower($character);
+    }
+    
+    return implode('', $characters); //convert the array into a string and return the string
+}
+public function __autoload() {
+    require_once camelCaseToUnderscore($class) . '.class.php';
+}
+
+?>
+
